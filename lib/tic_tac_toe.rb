9e752fffafca59fdb9dfc6c7579758aa430aa3f9
energetic-certificate-0804 <mjbugay@yahoo.com>
2020-09-02 00:53:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,4 +30,10 @@ def move(index, current_player = "X")
   @board[index] = current_player
 end
 
+def position_taken?(index)
+  !(@board[index].nil? || @board[index] == " ")
+end
 
+def valid_move?(index)
+  index.between?(0,8) && !position_taken?(index)
+end
