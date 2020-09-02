f19c79c9f79437da45f6be1d2f9908a8c2e00188
energetic-certificate-0804 <mjbugay@yahoo.com>
2020-09-02 00:59:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,7 +62,14 @@ def turn_count
 end
 
 def current_player
-  
+    num_turns = turn_count
+  if num_turns % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+  return player
+end
   
 
 end
